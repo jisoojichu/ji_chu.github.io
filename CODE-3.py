d68d3010e0f1a3e522bc5f2f2e50cb6310ce3e00
ji.chu <wqs911@naver.com>
2024-05-22 14:17:35 +0900

    CODE-3.py

--- CODE-3.py
+++ CODE-3.py
@@ -1,12 +1,18 @@
-import collections
-def solution(participant, completion):
-    answer = collections.Counter(participant) - collections.Counter(completion)
-    return list(answer.keys())[0]
+def solution(arr):
+    result = []
+    prev_num = arr[0]
+    result.append(prev_num)
+    
+    for num in arr[1:]:
+        if num!= prev_num:
+            result.append(num)
+            prev_num = num
+            
+    return result
 
-이 코드는 Counter 객체를 사용하여 문제를 해결하고 있습니다. Counter 객체는 시퀀스나 매핑 객체로부터 생성될 수 있으며, 항목별 개수를 자동으로 세어주는 기능을 제공합니다. 여기서는 participant와 completion 리스트로부터 각각 Counter 객체를 생성하고 이들의 차를 구하여 미완주자 선수의 이름을 찾고 있습니다.
+이 코드는 배열 내의 연속된 중복 값을 제거하는 함수를 구현하는 것입니다.
 
-collections.Counter(participant)는 참여자 목록 participant에 대하여 각 선수의 등장 횟수를 센 Counter 객체를 생성합니다. 마찬가지로 collections.Counter(completion)는 완주자 목록 completion에 대하여 각 선수의 완주 횟수를 센 Counter 객체를 생성합니다.
-
-이후 collections.Counter(participant) - collections.Counter(completion) 연산으로 두 Counter 객체 간의 차이를 구할 수 있습니다. 이 차이는 참여했으나 완주하지 않은 선수의 이름으로 구성됩니다.
-
-마지막으로 list(answer.keys())[0]를 통해 Counter 객체의 key들 중 첫 번째 key를 선택하여 반환하게 됩니다. 이 key가 바로 미완주자 선수의 이름이 됩니다.
+solution() 함수에서는 입력으로 받은 배열 arr을 순회하며 중복되는 값들을 제거합니다.
+첫 번째 값은 무조건 리스트에 추가되며, 이후 값들부터는 이전 값(prev_num)과 비교하여 다르면 리스트에 추가하고 이전 값으로 설정합니다.
+이렇게 하면 결과적으로 중복되는 값들이 모두 제거된 배열이 만들어집니다.
+예를 들어, solution([1, 2, 3, 2, 4]) 함수가 호출되면 반환값은 [1, 2, 3, 4]가 됩니다. 왜냐하면 [2, 3, 2, 4] 중 2와 3만이 이전 값과 다르기 때문입니다. 이 알고리즘은 매우 효율적이며 시간 복잡도는 O(n)입니다.
